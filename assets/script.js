0e03beadf9925f218842ef3c587bab7459ce6cd3
elismountain <echospb@mail.ru>
2024-01-17 22:27:05 +0000

    added  5 days cards

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ const apiKey = '30ced98bf7b47a3e0d5002206004e34b';
 
 function fetchWeather(search) {
   let queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=${apiKey}`;
-
+  
   fetch(queryURL)
     .then(function (response) {
         return response.json()
@@ -21,55 +21,57 @@ function fetchWeather(search) {
         })
         .then(function (data) {
             console.log(data);
-            const currentWeather = data.list;
-            displayCurrentWeather(data.list[0]);
-
-            const fiveDayForecast = currentWeather.filter(function (data) {
-              return data.dt_txt.includes('12:00:00');
-            });
-           console.log(fiveDayForecast);
-
-           for (let i = 0; i < fiveDayForecast.length; i++) {
-            const day = fiveDayForecast[i];
-
-
-
-          //   <div class="card">
-          //   <div class="card-body">
-          //     <h3 id="card-title"></h3>
-          //     <img id="icon">
-          //     <p id="temp"></p>
-          //     <p id="wind"></p>
-          //     <p id="humidity"></p>
-          //   </div>
-          // </div>
-
-           }
+            displayForecast(data);
         })
     })
 };
 
 function displayCurrentWeather(currentWeather) {
-  const iconUrl = `https://openweathermap.org${currentWeather.weather.icon}.png` // TBC
+  const iconUrl = `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}.png` 
+  console.log(iconUrl)
   const icon = $('#icon').attr('src', iconUrl);
-  const temp = $('#temp').text(`Temperature: ${currentWeather.main.temp} C`);
+  const temp = $('#temp').text(`Temperature: ${currentWeather.main.temp} °C`);
   const wind = $('#wind').text(`Wind: ${currentWeather.wind.speed} kph`);
   const humidity = $('#humidity').text(`Humidity: ${currentWeather.main.humidity} %`);
 }
 
+function displayForecast(data) {
+  const currentWeather = data.list;
+  displayCurrentWeather(data.list[0]);
+            
+  const fiveDayForecast = currentWeather.filter(function (data) {
+    return data.dt_txt.includes('12:00:00');
+  });
+  console.log(fiveDayForecast);
 
+  $('#forecast').empty();
 
+  for (let i = 0; i < fiveDayForecast.length; i++) {
+    const day = fiveDayForecast[i];
+    const cardCol = $('<div>').attr('class', 'col-md');
+    const forecastCard = $('<div>').attr('class', 'card');
+    const forecastBody = $('<div>').attr('class', 'card-body');
+    const forecastIconUrl = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+    const forecastIcon = $('<img>').attr('src', forecastIconUrl);
+    const forecastTitle = $('<h5>').attr('class', 'card-title').text(dayjs(day.dt_txt).format('DD-MM-YYYY')); // Year optional
+    const forecastTemp = $('<p>').text(`Temp: ${day.main.temp} °C`);
+    const forecastWind = $('<p>').text(`Wind: ${day.wind.speed} kph`);
+    const forecastHumidity = $('<p>').text(`Humidity: ${day.main.humidity} %`);
 
-    $('#search-button').on('click', function(e) {
-      e.preventDefault();
+    $('#forecast').append(cardCol);
+    cardCol.append(forecastCard);
+    forecastCard.append(forecastBody);
+    forecastBody.append(forecastTitle, forecastIcon, forecastTemp, forecastWind, forecastHumidity);
+  }
+}
 
-      const search = $('#search-input').val().trim();
-      $('#today').attr('class', 'mt-3');
-      fetchWeather(search);
+$('#search-button').on('click', function(e) {
+  e.preventDefault();
 
-
-
-    })
+  const search = $('#search-input').val().trim();
+  $('#today').attr('class', 'mt-3');
+  fetchWeather(search);
+})
 
 
 
